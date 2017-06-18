26e11c3755331f0632b6e28747532d7c31cd0b24
Tobbe <contact@tobbexiv.de>
2017-06-18 14:12:04 +0200

    Resolve #10 add translations for nl_NL and de_DE.

---
+++ languages/de_DE/messages.php
@@ -0,0 +1,30 @@
+<?php
+/* 
+ * de_DE translation for tobbe/widget-includer
+ * Last update at 2017-06-18 11:16+0000
+ */
+return array (
+  'Title Size' => 'Titelgröße',
+  'Loading widget ...' => 'Widget wird geladen ...',
+  'Widget does not exist!' => 'Das Widget existiert nicht!',
+  'Widget' => 'Widget',
+  'This widget is disabled. No one will be able to see it.' => 'Dieses Widget ist deaktiviert. Niemand wir es sehen können.',
+  'Comment' => 'Kommentar',
+  'Update' => 'Aktualisieren',
+  'Add Widget' => 'Widget hinzufügen',
+  'Widget placeholder.' => 'Platzhalter für Widget.',
+  'There was an error loading the widget!' => 'Beim Laden des Widgets trat ein Fehler auf!',
+  'Use the widget includer editor plugin' => 'Nutzung des Widget Includer Editor Plugins',
+  'You are not authorized to see this widget!' => 'Du hast keine Berechtigung das Widget zu sehen!',
+  'This comment is displayed in the editor, but not evaluated by the widget. So you can enter information which helps you to identify the widget.' => 'Dieser Kommentar wird im Editor angezeigt, aber nicht durch das Widget genutzt. Du kannst somit Informationen eingeben, welche Dir helfen das Widget zu identifizieren.',
+  'Preview' => 'Vorschau',
+  'Widget is disabled!' => 'Das Widget ist deaktiviert!',
+  'Title' => 'Titel',
+  'Widget id: %widgetId%' => 'Widget ID: %widgetId%',
+  'Hide Title' => 'Titel verbergen',
+  'Render placeholder instead of content in preview' => 'Platzhalter statt Inhalt in der Vorschau darstellen',
+  'Widget comment: %comment%' => 'Widget Kommentar: %comment%',
+  'Insert/edit widget' => 'Widget einfügen/bearbeiten',
+  'Id' => 'ID',
+  'Cancel' => 'Abbrechen',
+);
---
+++ languages/nl_NL/messages.php
@@ -0,0 +1,30 @@
+<?php
+/* 
+ * nl_NL translation for tobbe/widget-includer
+ * Last update at 2017-06-18 11:16+0000
+ */
+return array (
+  'Title Size' => 'Titel Grootte',
+  'Loading widget ...' => 'Widget laden...',
+  'Widget does not exist!' => 'Deze widget bestaat niet!',
+  'Widget' => 'Widget',
+  'This widget is disabled. No one will be able to see it.' => 'Deze widget is uitgeschakeld. Niemand kan deze widget zien.',
+  'Comment' => 'Notitie',
+  'Update' => 'Update',
+  'Add Widget' => 'Widget Toevoegen',
+  'Widget placeholder.' => 'Widget placeholder',
+  'There was an error loading the widget!' => 'Er was een fout bij het laden van deze widget!',
+  'Use the widget includer editor plugin' => '',
+  'You are not authorized to see this widget!' => 'Je bent niet bevoegd om deze widget te zien!',
+  'This comment is displayed in the editor, but not evaluated by the widget. So you can enter information which helps you to identify the widget.' => 'Deze notitie wordt weergeven in deze editor maar wordt genegeerd bij het weergeven van de widget. Gebruik de notitie om een widget te identificeren.',
+  'Preview' => 'Voorbeeld',
+  'Widget is disabled!' => 'Widget is uitgeschakeld!',
+  'Title' => 'Titel',
+  'Widget id: %widgetId%' => 'Widget id: %widgetId%',
+  'Hide Title' => 'Verberg Titel',
+  'Render placeholder instead of content in preview' => 'Laat de widget placeholder zien in plaats van de content in de voorbeeldweergave',
+  'Widget comment: %comment%' => 'Widget-notitie: %comment%',
+  'Insert/edit widget' => 'Widget toevoegen/wijzigen',
+  'Id' => 'Id',
+  'Cancel' => 'Annuleren',
+);
